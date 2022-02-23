cc35cee95abe53840655e9ac3e2d8d78c5be2e19
Cassio Jaber <jcjaber@gmail.com>
2022-02-23 11:49:12 -0300

    setup maps javascript api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log('Hello map!')
+let map;
+
+function initMap() {
+  let mapOptions = {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  };
+
+  map = new google.maps.Map(document.getElementById("map"),mapOptions);
+}
